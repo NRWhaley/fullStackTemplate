de16d3316286afa1e34ef9ba5d3be853300fa507
NWhaley <nicholas.whaley@gmail.com>
2021-09-17 17:42:09 -0700

    add API call basic

--- fST/src/components/Main.jsx
+++ fST/src/components/Main.jsx
@@ -1,3 +1,4 @@
+import { response } from 'express';
 import React, { useState, useEffect } from 'react';
 
 
@@ -5,16 +6,23 @@ function App () {
 
 const [count, setCount] = useState(0)
 
+const [info, setInfo] = useState('')
 	useEffect(() => {
-		document.title = 'You clicked ${count} times'
+	//	document.title = 'You clicked ${count} times?'
+
+	fetch('https://jsonplaceholder.typicode.com/todos/1')
+	.then(response => response.json())
+	.then(json => 
+		setInfo(json.title))
 	})
 
 
 		return (
 		<div>
-		<h1>You've clicked {count} times</h1>
+		<h1>You have clicked {count} times</h1>
 		<button onClick={()=> 
 			setCount(count+1)}>Hi</button>
+		<h2>This is a response {info}</h2>
 		</div>
 		
 		)
